622c47c406cbcba7a752940781c86711e8c5b189
Nicolas Torres <Nicolas.torres-p@mail.escuelaing.edu.co>
2021-04-13 23:09:18 -0500

    login y parte del codigo base

--- src/main/java/edu/eci/cvds/view/LoginBean.java
+++ src/main/java/edu/eci/cvds/view/LoginBean.java
@@ -1,4 +1,37 @@
 package edu.eci.cvds.view;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
+@ManagedBean(name = "login")
+@SessionScoped
 public class LoginBean extends BasePageBean{
+    private String correo;
+    private String contrasena;
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * set user email 
+     * @param correo
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * Method to get user
+     * @param contrasena, represents user password
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    /**
+     * Method  to get user password
+     * @return String
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
 }
