57dad05ccf9fb5bea2ea1aac07e5b3c2bae71d6b
Nicolas Torres <Nicolas.torres-p@mail.escuelaing.edu.co>
2021-04-15 08:19:40 -0500

    new push login

--- src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
+++ src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
@@ -33,23 +33,23 @@ public class GuiceContextListener {
             @Override
             protected void initialize() {
 
-                install(JdbcHelper.PostgreSQL);
-                setEnvironmentId("development");
-                setClassPathResource("mybatis-config.xml");
+                // install(JdbcHelper.PostgreSQL);
+                // setEnvironmentId("development");
+                // setClassPathResource("mybatis-config.xml");
 				
-				//binding
-                //login
-                bind(Log.class).to(ShiroLog.class);
-                //DAO
-				bind(CategoriasDAO.class).to(MyBatisCategoriasDAO.class);
-				bind(NecesidadesDAO.class).to(MyBatisNecesidadesDAO.class);
-                bind(OfertasDAO.class).to(MyBatisOfertasDAO.class);
-				bind(RespuestasDAO.class).to(MyBatisRespuestasDAO.class);
-                //Impl
-				bind(CategoriasService.class).to(CategoriasServiceImpl.class);
-                bind(NecesidadesService.class).to(NecesidadesServiceImpl.class);
-                bind(OfertasService.class).to(OfertasServiceImpl.class);
-                bind(RespuestasService.class).to(RespuestasServiceImpl.class);
+				// //binding
+                // //login
+                // bind(Log.class).to(ShiroLog.class);
+                // //DAO
+				// bind(CategoriasDAO.class).to(MyBatisCategoriasDAO.class);
+				// bind(NecesidadesDAO.class).to(MyBatisNecesidadesDAO.class);
+                // bind(OfertasDAO.class).to(MyBatisOfertasDAO.class);
+				// bind(RespuestasDAO.class).to(MyBatisRespuestasDAO.class);
+                // //Impl
+				// bind(CategoriasService.class).to(CategoriasServiceImpl.class);
+                // bind(NecesidadesService.class).to(NecesidadesServiceImpl.class);
+                // bind(OfertasService.class).to(OfertasServiceImpl.class);
+                // bind(RespuestasService.class).to(RespuestasServiceImpl.class);
             }
         });
 
