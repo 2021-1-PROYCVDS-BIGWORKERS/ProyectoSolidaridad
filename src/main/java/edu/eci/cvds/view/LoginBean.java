c27cf791e632737bfda9ca555fe4b63acb08f0a4
Nicolas Torres <Nicolas.torres-p@mail.escuelaing.edu.co>
2021-04-15 08:20:16 -0500

    login push bean

--- src/main/java/edu/eci/cvds/view/LoginBean.java
+++ src/main/java/edu/eci/cvds/view/LoginBean.java
@@ -2,11 +2,14 @@ package edu.eci.cvds.view;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+
+
 @ManagedBean(name = "login")
 @SessionScoped
 public class LoginBean extends BasePageBean{
     private String correo;
     private String contrasena;
+    
     public String getCorreo() {
         return correo;
     }
